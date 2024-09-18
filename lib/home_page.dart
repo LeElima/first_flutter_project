0ddef49b5c3f1dc28ff4e0844d3db07a796169bd
LeElima <leandrolimaa2008@hotmail.com>
2024-09-18 08:57:35 -0300

    Inserido coluna

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -26,8 +26,17 @@ class HomePageState extends State<HomePage>{
           CustomSwitch()
         ],
       ),
-      body: Center(
-        child: CustomSwitch(),
+      body:Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Contador : ${counter}"),
+            CustomSwitch(),
+            
+          ],
+        ),
       ),
       // body: Center(
       //       child: GestureDetector(
